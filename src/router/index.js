c820f2065df8face5b0548493989db28db5a01c1
pugocoder <57970164+pugocoder@users.noreply.github.com>
2022-06-24 16:53:52 +0300

    init: create and configure vue 3 project

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    // component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
