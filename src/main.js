863faf9f308fd3ad689192d6593c7ea120384f55
pugocoder <57970164+pugocoder@users.noreply.github.com>
2022-06-26 10:55:37 +0300

    feat: add favicon

--- src/main.js
+++ src/main.js
@@ -1,19 +1,5 @@
 import { createApp } from "vue";
-import { createStore } from "vuex";
 import App from "./App.vue";
 import router from "./router";
 
-const store = createStore({
-  state() {
-    return {
-      likedPlaces: [],
-    };
-  },
-  mutations: {
-    likePlace(id) {
-      this.state.likedPlaces.push(id);
-    },
-  },
-});
-
-createApp(App).use(router).use(store).mount("#app");
+createApp(App).use(router).mount("#app");
